1cba79082d9226b4aafecb149671a73e84396c89
Philip Riecks <mail@philipriecks.de>
2021-08-24 07:19:11 +0200

    migrate back to Jakarta EE 8

--- jakarta-ee-example/src/main/java/de/rieckpil/blog/SampleResource.java
+++ jakarta-ee-example/src/main/java/de/rieckpil/blog/SampleResource.java
@@ -1,8 +1,8 @@
 package de.rieckpil.blog;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.core.Response;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
 
 @Path("sample")
 public class SampleResource {
