666f0f20981b8dd78f9f6f5e2f59ecd06906d066
Philip Riecks <mail@philipriecks.de>
2021-11-02 06:15:40 +0100

    use recent LocalStack version

--- spring-boot-example/src/test/java/de/rieckpil/blog/localstack/ImageUploadEventListenerIT.java
+++ spring-boot-example/src/test/java/de/rieckpil/blog/localstack/ImageUploadEventListenerIT.java
@@ -1,19 +1,19 @@
 package de.rieckpil.blog.localstack;
 
+import java.io.IOException;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.BindMode;
 import org.testcontainers.containers.localstack.LocalStackContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
-import java.io.IOException;
-
 import static org.testcontainers.containers.BindMode.READ_ONLY;
+import static org.testcontainers.containers.localstack.LocalStackContainer.Service;
 import static org.testcontainers.containers.localstack.LocalStackContainer.Service.S3;
 import static org.testcontainers.containers.localstack.LocalStackContainer.Service.SQS;
 
@@ -23,9 +23,8 @@ public class ImageUploadEventListenerIT {
 
   @Container
   static LocalStackContainer localStack =
-    new LocalStackContainer(DockerImageName.parse("localstack/localstack:0.12.6"))
-      .withServices(S3, SQS)
-      .withEnv("DEFAULT_REGION", "eu-central-1")
+    new LocalStackContainer(DockerImageName.parse("localstack/localstack:0.12.19"))
+      .withServices(Service.S3, Service.SQS)
       .withClasspathResourceMapping("/localstack", "/docker-entrypoint-initaws.d", READ_ONLY)
       .waitingFor(Wait.forLogMessage(".*Initialized\\.\n", 1));
 
@@ -34,6 +33,8 @@ public class ImageUploadEventListenerIT {
     registry.add("cloud.aws.sqs.enabled", () -> true);
     registry.add("cloud.aws.s3.endpoint", () -> localStack.getEndpointOverride(S3));
     registry.add("cloud.aws.sqs.endpoint", () -> localStack.getEndpointOverride(SQS));
+    registry.add("cloud.aws.credentials.access-key", () -> localStack.getAccessKey());
+    registry.add("cloud.aws.credentials.secret-key", () -> localStack.getSecretKey());
   }
 
   @Test
