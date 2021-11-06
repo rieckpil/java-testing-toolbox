c1f1cf5c3f16992c8fdab1a027cf121f86d0e1ed
jayant019 <59733042+jayant019@users.noreply.github.com>
2021-11-06 21:38:42 +0530

    added script and sample results

---
+++ gatling-example/user-files/simulations/customerRequest/loadtest.scala
@@ -0,0 +1,36 @@
+package customerRequest
+
+import scala.concurrent.duration._
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.jdbc.Predef._
+
+class loadtest extends Simulation {
+
+	val httpProtocol = http
+		.baseUrl("http://localhost:8000")
+		.inferHtmlResources()
+		.acceptHeader("*/*")
+		.acceptEncodingHeader("gzip, deflate")
+		.acceptLanguageHeader("en-GB,en-US;q=0.9,en;q=0.8,hi;q=0.7")
+		.doNotTrackHeader("1")
+		.userAgentHeader("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36")
+
+	val headers_0 = Map(
+		"Sec-Fetch-Dest" -> "empty",
+		"Sec-Fetch-Mode" -> "cors",
+		"Sec-Fetch-Site" -> "none",
+		"sec-ch-ua" -> """Google Chrome";v="95", "Chromium";v="95", ";Not A Brand";v="99""",
+		"sec-ch-ua-mobile" -> "?0",
+		"sec-ch-ua-platform" -> "Windows")
+
+
+
+	val scn = scenario("loadtest")
+		.exec(http("request_0")
+			.get("/api/customers")
+			.headers(headers_0))
+
+	 setUp(scn.inject(rampUsers(10).during(2.minutes))).maxDuration(5.minutes).protocols(httpProtocol)
+}
