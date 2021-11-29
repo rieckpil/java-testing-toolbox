6583c6238a2519387f8296f452b4f9a228ae872b
Philip Riecks <mail@philipriecks.de>
2021-11-29 16:50:24 +0100

    include Gatling example within the Spring Boot application
    Gatling 3.7 now ships with a Java DSL. This simplifies the overall setup

--- spring-boot-example/README.md
+++ spring-boot-example/README.md
@@ -1,5 +1,7 @@
 # Spring Boot Sample Application
 
+You can find the source code for the various testing tools & libraries within `src/test/java/de/rieckpil/blog`. Each tool & library has its dedicated Java package.
+
 ## Prerequisites
 
 - Java 11: `java -version`
@@ -9,7 +11,12 @@
 
 1. Execute all tests with `mvn verify`
 
-## Run the application
+## Start the application
 
 1. Start the required infrastructure with: `docker-compose up`
 2. Run the Spring Boot application with: `./mvnw spring-boot:run` (Mac/Linux) or `mvnw.cmd spring-boot:run` (Windows)
+
+## Run the Gatling performance tests
+
+1. Start the application as mentioned above
+2. Run `./mvnw gatling:test` (Mac/Linux) or `mvnw.cmd gatling:test` (Windows)
---
+++ spring-boot-example/src/test/java/de/rieckpil/blog/gatling/CustomerRequestSimulation.java
@@ -0,0 +1,34 @@
+package de.rieckpil.blog.gatling;
+
+import java.time.Duration;
+import java.util.UUID;
+
+import io.gatling.javaapi.core.ScenarioBuilder;
+import io.gatling.javaapi.core.Simulation;
+import io.gatling.javaapi.http.HttpProtocolBuilder;
+
+import static io.gatling.javaapi.core.CoreDsl.constantUsersPerSec;
+import static io.gatling.javaapi.core.CoreDsl.scenario;
+import static io.gatling.javaapi.http.HttpDsl.http;
+
+public class CustomerRequestSimulation extends Simulation {
+
+  HttpProtocolBuilder httpProtocol = http
+    .baseUrl("http://localhost:8080")
+    .acceptHeader("application/json")
+    .userAgentHeader("Gatling/Performance Test");
+
+  ScenarioBuilder scn = scenario("Load Test HTTP POST API")
+    .exec(http("spring-boot-backend-request")
+      .post("/api/customers")
+    );
+
+  {
+    setUp(scn.injectOpen(constantUsersPerSec(50).during(Duration.ofSeconds(15)).randomized()))
+      .protocols(httpProtocol);
+  }
+
+  public String randomUsername() {
+    return UUID.randomUUID().toString();
+  }
+}
