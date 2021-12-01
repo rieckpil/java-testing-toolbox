f8aadc7e713f0533acc9349a78773ae7b19ab4e5
Philip Riecks <mail@philipriecks.de>
2021-12-01 06:45:13 +0100

    user a Feeder to add random usernames

--- spring-boot-example/src/test/java/de/rieckpil/blog/gatling/CustomerRequestSimulation.java
+++ spring-boot-example/src/test/java/de/rieckpil/blog/gatling/CustomerRequestSimulation.java
@@ -1,7 +1,12 @@
 package de.rieckpil.blog.gatling;
 
 import java.time.Duration;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.UUID;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 import io.gatling.javaapi.core.ScenarioBuilder;
 import io.gatling.javaapi.core.Simulation;
@@ -9,10 +14,16 @@ import io.gatling.javaapi.http.HttpProtocolBuilder;
 
 import static io.gatling.javaapi.core.CoreDsl.StringBody;
 import static io.gatling.javaapi.core.CoreDsl.constantUsersPerSec;
+import static io.gatling.javaapi.core.CoreDsl.feed;
 import static io.gatling.javaapi.core.CoreDsl.scenario;
 import static io.gatling.javaapi.http.HttpDsl.http;
 
 public class CustomerRequestSimulation extends Simulation {
+
+  Iterator<Map<String, Object>> feeder =
+    Stream.generate((Supplier<Map<String, Object>>) ()
+      -> Collections.singletonMap("username", UUID.randomUUID().toString())
+    ).iterator();
 
   HttpProtocolBuilder httpProtocol = http
     .baseUrl("http://localhost:8080")
@@ -20,18 +31,15 @@ public class CustomerRequestSimulation extends Simulation {
     .userAgentHeader("Gatling/Performance Test");
 
   ScenarioBuilder scn = scenario("Load Test HTTP POST API")
+    .feed(feeder)
     .exec(http("spring-boot-backend-request")
       .post("/api/customers")
       .header("Content-Type", "application/json")
-      .body(StringBody(session -> "{ \"username\": \"" + randomUsername() + "\" }"))
+      .body(StringBody("{ \"username\": ${username} }"))
     );
 
   public CustomerRequestSimulation() {
     setUp(scn.injectOpen(constantUsersPerSec(50).during(Duration.ofSeconds(15)).randomized()))
       .protocols(httpProtocol);
-  }
-
-  public String randomUsername() {
-    return UUID.randomUUID().toString();
   }
 }
