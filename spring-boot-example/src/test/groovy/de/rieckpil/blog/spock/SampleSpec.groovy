fba3351bd0db94210acc3bda010a088d208877cd
Philip Riecks <mail@philipriecks.de>
2021-12-18 10:02:35 +0100

    add more Spock examples

--- spring-boot-example/src/test/groovy/de/rieckpil/blog/spock/SampleSpec.groovy
+++ spring-boot-example/src/test/groovy/de/rieckpil/blog/spock/SampleSpec.groovy
@@ -1,5 +1,6 @@
 package de.rieckpil.blog.spock
 
+import spock.lang.Ignore
 import spock.lang.Specification
 
 class SampleSpec extends Specification {
@@ -13,5 +14,29 @@ class SampleSpec extends Specification {
 
     then:
     result == "DUKE"
+  }
+
+  def "should fail with NPE when checking length of null String"() {
+    given:
+    String username = null
+
+    when:
+    def result = username.length()
+
+    then:
+    thrown(NullPointerException)
+  }
+
+  @Ignore("Showcase only")
+  def "should fail with readable assertion error in the log"() {
+    given:
+    int firstValue = 1
+    int secondValue = 42
+
+    when:
+    int expectedResult = 100
+
+    then:
+    Math.max(firstValue, secondValue) == expectedResult
   }
 }
