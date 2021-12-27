d9296ccb050b359f0cd11ea87c1da61ed94bb3f1
Philip Riecks <mail@philipriecks.de>
2021-12-27 16:05:27 +0100

    separate Spock's stubs and mocks

--- spring-boot-example/src/test/groovy/de/rieckpil/blog/spock/UserRegistrationServiceSpec.groovy
+++ spring-boot-example/src/test/groovy/de/rieckpil/blog/spock/UserRegistrationServiceSpec.groovy
@@ -7,19 +7,29 @@ import spock.lang.Specification
 
 class UserRegistrationServiceSpec extends Specification {
 
-  def userRepositoryMock = Mock(UserRepository)
-
-  def "should return the user from the DB if the user already exists (Mock)"() {
+  def "should return the existing user from the database"() {
     given:
     def savedUser = new User("Bob")
-    def cut = new UserRegistrationService(userRepositoryMock)
-    userRepositoryMock.findByUsername("Bob") >> savedUser
+    def userRepositoryStub = Stub(UserRepository)
+    def cut = new UserRegistrationService(userRepositoryStub)
+    userRepositoryStub.findByUsername("Bob") >> savedUser
 
     when:
     def result = cut.registerUser("Bob")
 
     then:
     result == savedUser
-    0 * userRepositoryMock.save
+  }
+
+  def "should save new user to the database"() {
+    given:
+    def userRepositoryMock = Mock(UserRepository)
+    def cut = new UserRegistrationService(userRepositoryMock)
+
+    when:
+    def result = cut.registerUser("Bob")
+
+    then:
+    1 * userRepositoryMock.save({it -> it.username == "Bob"})
   }
 }
