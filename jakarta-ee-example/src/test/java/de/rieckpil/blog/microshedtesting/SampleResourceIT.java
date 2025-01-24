9fa8b408419d97bf11085ac04319fafcf9ccf2c5
Philip Riecks <mail@philipriecks.de>
2025-01-24 13:16:02 +0100

    disable test

--- jakarta-ee-example/src/test/java/de/rieckpil/blog/microshedtesting/SampleResourceIT.java
+++ jakarta-ee-example/src/test/java/de/rieckpil/blog/microshedtesting/SampleResourceIT.java
@@ -3,6 +3,7 @@ package de.rieckpil.blog.microshedtesting;
 import jakarta.json.Json;
 import jakarta.json.JsonObject;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.microshed.testing.SharedContainerConfig;
 import org.microshed.testing.jupiter.MicroShedTest;
@@ -15,6 +16,7 @@ import static org.mockserver.model.HttpResponse.response;
 import static org.mockserver.model.MediaType.JSON_UTF_8;
 
 @MicroShedTest
+@Disabled("Rest Assured is not yet compatible with Jakarta EE 10")
 @SharedContainerConfig(SampleApplicationConfig.class)
 class SampleResourceIT {
 
